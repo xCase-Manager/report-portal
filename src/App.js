bc3a34b33bf1767ca32ddaafbfc2d874a615c0bc
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-09-05 16:39:59 +0200

    modified App settings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Dashboard from './components/dashboard.component';
+import Execution from './components/execution.component';
 
 function App() {
-  return (
+  return (<Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/dashboard"}>Report Portal</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/dashboard"}>Dashboard</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/execution"}>Execution</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={Dashboard} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/execution" component={Execution} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
   );
 }
 
