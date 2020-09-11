47cd92040c2ddc5e489b69dba4476e87d9b9c4c1
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-09-11 16:15:39 +0200

    added HTTP service to feed executions list

--- src/shared/mock-execution-service.js
+++ src/shared/mock-execution-service.js
@@ -1,16 +1,26 @@
 class ExecutionService {
+
     constructor() {
-      this.executions = [
-        {id:"1", testcaseId:"AAA01", state:"fail"},
-        {id:"2", testcaseId:"AAA01", state:"success"},
-        {id:"3", testcaseId:"AAA02", state:"in queue"},
-        {id:"4", testcaseId:"AAA02", state:"executing"},
-        {id:"5", testcaseId:"AAA03", state:"not executed"}
-      ];
+      this.executions = [];
     }
+
     async retrieveExecutions() {
-        return Promise.resolve(this.executions);
+        return fetch("http://localhost:5000/api/executions")
+        .then(response => {
+            if (!response.ok) {
+            this.handleResponseError(response);
+            }
+            return response.json();
+        })
+        .then(json => {
+            this.executions = json;
+            return json;
+        })
+        .catch(error => {
+            this.handleError(error);
+        });
     }
+
     async getExecution(executionLink) {
       for(var i = 0; i < this.executions.length; i++) {
         if ( this.executions[i].link === executionLink) {
@@ -19,18 +29,29 @@ class ExecutionService {
       }
       return null;
     }
+
     async createExecution(execution) {
       console.log("ExecutionService.createExecution():");
       console.log(execution);
       return Promise.resolve(execution);
     }
+
     async deleteExecution(executionId) {
       console.log("ExecutionService.deleteExecution():");
       console.log("execution ID:" + executionId);
     }
+
     async updateExecution(execution) {
       console.log("ExecutionService.updateExecution():");
       console.log(execution);
+    }
+
+    handleResponseError(response) {
+        throw new Error("HTTP error, status = " + response.status);
+    }
+
+    handleError(error) {
+        console.log(error.message);
     }
   }
   export default ExecutionService;
