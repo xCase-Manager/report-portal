2d74c5e8fab276c664c3bf04a7f10977af37c051
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-09-15 18:43:15 +0200

    added api remote creation for execution component

--- src/components/execution.component.js
+++ src/components/execution.component.js
@@ -1,21 +1,38 @@
 import React, { Component } from "react";
+import ExecutionService from '../shared/mock-execution-service';
 
 export default class Execution extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.executionService = new ExecutionService();    
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const execution = new FormData(event.target);
+        this.executionService.createExecution(execution).then(execution => {
+              console.log(execution);
+            }
+        );
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <h3>New Execution</h3>
+                <div className="form-group">
+                    <label>Execution ID</label>
+                    <input type="text" className="form-control" placeholder="Execution ID" />
+                </div>
                 <div className="form-group">
                     <label>Test Case ID</label>
                     <input type="text" className="form-control" placeholder="Test case ID" />
                 </div>
                 <div className="form-group">
-                    <label>Tag</label>
-                    <input type="text" className="form-control" placeholder="Tag" />
-                </div>
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <label>Status</label>
+                    <input type="text" className="form-control" placeholder="Enter an execution status" />
                 </div>        
                 <button type="submit" className="btn btn-primary btn-block">Create</button>               
             </form>
