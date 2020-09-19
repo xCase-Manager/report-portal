6235e49ee8400ebcebdfef8a6ed3c9446aaf98d8
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-09-19 15:58:03 +0200

    added form submit handler

--- src/components/execution.component.js
+++ src/components/execution.component.js
@@ -5,37 +5,49 @@ export default class Execution extends Component {
 
     constructor(props) {
         super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.executionService = new ExecutionService();    
+        this.executionService = new ExecutionService();   
     }
 
-    handleSubmit(event) {
-        event.preventDefault();
-        const execution = new FormData(event.target);
-        this.executionService.createExecution(execution).then(execution => {
-              console.log(execution);
+    componentDidMount() {
+        const formElem = document.querySelector('form');
+
+        formElem.addEventListener('submit', (e) => {
+            e.preventDefault();
+            new FormData(formElem);
+        });
+        
+        formElem.addEventListener('formdata', (e) => {
+            let data = e.formData;
+            for (var value of data.values()) {
+              console.log(value);
             }
-        );
+           
+            this.executionService.createExecution(data).then(execution => {
+                console.log(execution);
+              }
+          );
+        });   
     }
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
+            <form encType="multipart/form-data" method="post">
                 <h3>New Execution</h3>
                 <div className="form-group">
                     <label>Execution ID</label>
-                    <input type="text" className="form-control" placeholder="Execution ID" />
+                    <input name="id" type="number" className="form-control" placeholder="Execution ID" />
                 </div>
                 <div className="form-group">
                     <label>Test Case ID</label>
-                    <input type="text" className="form-control" placeholder="Test case ID" />
+                    <input name="testcaseId" type="text" className="form-control" placeholder="Test case ID" />
                 </div>
                 <div className="form-group">
                     <label>Status</label>
-                    <input type="text" className="form-control" placeholder="Enter an execution status" />
+                    <input name="status" type="text" className="form-control" placeholder="Enter an execution status" />
                 </div>        
                 <button type="submit" className="btn btn-primary btn-block">Create</button>               
             </form>
+            
         );
     }
 }
