32f09332e33eeda7dd02909e78e2e62b5e852548
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-09-28 11:03:58 +0200

    added data and date series generator for mock service

--- src/shared/mock-execution-service.js
+++ src/shared/mock-execution-service.js
@@ -105,8 +105,61 @@ class ExecutionService {
         label: 'AAA04',
         value: 6
       });
-    
       return Promise.resolve(data);
+    }
+
+    getData() {
+      let data = [];
+
+      data.push({
+        title: 'AAA01',
+        data: getRandomDateArray(150)
+      });
+    
+      data.push({
+        title: 'AAA02',
+        data: getRandomArray(20)
+      });
+    
+      data.push({
+        title: 'BBB01',
+        data: getRandomArray(10)
+      });
+    
+      data.push({
+        title: 'BBB02',
+        data: getRandomArray(6)
+      });
+    
+      return data;
     }
   }
   export default ExecutionService;
+
+
+// Data generation
+function getRandomArray(numItems) {
+  let names = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let data = [];
+  for(var i = 0; i < numItems; i++) {
+    data.push({
+      label: names[i],
+      value: Math.round(20 + 80 * Math.random())
+    });
+  }
+  return data;
+}
+
+// Create random array of objects (with date)
+function getRandomDateArray(numItems) {  
+  let data = [];
+  let baseTime = new Date('2020-04-15T00:00:00').getTime();
+  let dayMs = 24 * 60 * 60 * 1000;
+  for(var i = 0; i < numItems; i++) {
+    data.push({
+      time: new Date(baseTime + i * dayMs),
+      value: Math.round(20 + 80 * Math.random())
+    });
+  }
+  return data;
+}
