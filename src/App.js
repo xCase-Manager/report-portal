2e197b48ff7bdf4443a6a2b794172e4088533446
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-10-03 13:47:48 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -6,33 +6,38 @@ import Dashboard from './components/dashboard.component';
 import Execution from './components/execution.component';
 
 function App() {
-  return (<Router>
-    <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-        <div className="container">
-          <Link className="navbar-brand" to={"/dashboard"}>Report Portal</Link>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to={"/dashboard"}>Dashboard</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/execution"}>Execution</Link>
-              </li>
-            </ul>
+  return (
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={"/dashboard"}>Report Portal</Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/dashboard"}>Dashboard</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/execution"}>Execution</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Switch>
+              <Route exact path='/' component={Dashboard}  />
+              <Route exact path='/dashboard' component={Dashboard}  />
+              <Route path="/execution" component={Execution} />
+            </Switch>
           </div>
         </div>
-      </nav>
-      <div className="auth-wrapper">
-        <div className="auth-inner">
-          <Switch>
-            <Route exact path='/' component={Dashboard}  />
-            <Route exact path='/dashboard' component={Dashboard}  />
-            <Route path="/execution" component={Execution} />
-          </Switch>
-        </div>
       </div>
-    </div></Router>
+      <footer class="my-5 pt-5 text-muted text-center text-small">
+        <p class="mb-1">&copy; 2019-2020 TCM</p>
+      </footer>
+    </Router>
   );
 }
 export default App;
