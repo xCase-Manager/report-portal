4ae17526e4f672bf78b57975375505351a4572cc
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-11-22 19:45:52 +0100

    removed navigation component

--- src/App.js
+++ src/App.js
@@ -8,18 +8,6 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <div className="container">
-            <Link className="navbar-brand" to={"/dashboard"}>Report Portal</Link>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/dashboard"}>Dashboard</Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
         <div className="auth-wrapper">
           <div className="auth-inner">
             <Switch>
@@ -29,8 +17,8 @@ function App() {
           </div>
         </div>
       </div>
-      <footer class="my-5 pt-5 text-muted text-center text-small">
-        <p class="mb-1">&copy; 2019-2020 TCM</p>
+      <footer className="my-5 pt-5 text-muted text-center text-small">
+        <p className="mb-1">&copy; 2019-2020 TCM</p>
       </footer>
     </Router>
   );
