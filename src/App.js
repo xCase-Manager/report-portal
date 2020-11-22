c5185360f0e7e68e774071badb0d83f7d251ff68
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-11-22 21:19:16 +0100

    reset chart background

--- src/App.js
+++ src/App.js
@@ -8,16 +8,10 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <div className="auth-wrapper">
-          <div className="auth-inner">
             <Switch>
               <Route exact path='/' component={Dashboard}  />
             </Switch>
-          </div>
-        </div>
-        
-      </div>
-      
+          </div>     
     </Router>
   );
 }
